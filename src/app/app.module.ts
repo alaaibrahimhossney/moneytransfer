36bfa7eda1e72f130dfe296294862dd6be29e82a
Alaa Ibrahim <alaa.ibrahim.hossney@gmail.com>
2024-09-06 22:42:26 +0300

    create the main project structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
